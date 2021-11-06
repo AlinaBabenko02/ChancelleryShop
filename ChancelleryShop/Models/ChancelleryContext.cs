bb90d0083ff2b972bdcd21b63d13b64426d1d9bc
VladBurtsevych <vladm2323@gmail.com>
2021-11-06 21:48:55 +0200

    Small fix

--- ChancelleryShop/Models/ChancelleryContext.cs
+++ ChancelleryShop/Models/ChancelleryContext.cs
@@ -25,15 +25,6 @@ namespace ChancelleryShop
         public virtual DbSet<Receipt> Receipts { get; set; }
         public virtual DbSet<Shop> Shops { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-1LJ593T\\SQLEXPRESS;Database=Chancellery;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
